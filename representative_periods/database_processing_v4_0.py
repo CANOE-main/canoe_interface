fe31ed84804858cd1fc4b2282b373f54187edf35
Yamil Essus <essusyamil@gmail.com>
2026-08-06 17:08:06 -0400

    fix dsd processing when flatline is used
    Signed-off-by: Yamil Essus <essusyamil@gmail.com>

--- representative_periods/database_processing_v4_0.py
+++ representative_periods/database_processing_v4_0.py
@@ -263,12 +263,11 @@ def process_single_day_period(database: str, hours: list):
         # Get a running proportion sum of DSD
         df['run_sum'] = df['dsd'].cumsum()/df['dsd'].sum()
         # Get the smallest DSD above thresh to zero out actual table
-        thresh_dsd = df['dsd'].loc[df['run_sum'] < utils.config['dsd_threshold']].max()
-        
-        # There might be nothing under the threshold if using few rep days
-        if not pd.isna(thresh_dsd):
+        thresh_dsd = df["dsd"].loc[df["run_sum"] < utils.config["dsd_threshold"]].max()
+        thresh_dsd += 1e-12  # Small buffer to avoid floating point issues
 
-            thresh_dsd += 1e-12  # Small buffer to avoid floating point issues
+        # There might be nothing under the threshold if using few rep days
+        if not pd.isna(thresh_dsd) and thresh_dsd < df["dsd"].max():
 
             # Set to zero where the proportion exceeds the threshold
             # If there are duplicate dsd values on the threshold these are all left in
